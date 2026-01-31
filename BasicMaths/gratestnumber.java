8c8c754b7349d8293d63db62485ceea8a9bd9adc
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-01-31 22:21:18 +0530

    added

---
+++ BasicMaths/gratestnumber.java
@@ -0,0 +1,14 @@
+package BasicMaths;
+
+public class gratestnumber {
+
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if (a > b)
+            System.out.println(a + " is greater");
+        else
+            System.out.println(b + " is greater");
+    }
+
+}
