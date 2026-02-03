ef36be51f4d914e2ff54a18a6c0d6fbf53837aeb
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-03 12:50:20 +0530

    reverse

---
+++ Arrays/reverse.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class reverse {
+    public static void getReverse(int num[]) {
+        int first = 0;
+        int last = num.length - 1;
+        while (first < last) {
+            int temp = num[last];
+            num[last] = num[first];
+            num[first] = temp;
+
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int num[]={2,4,6,8,10};
+        getReverse(num);
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
+        }
+    
+    }
+}
