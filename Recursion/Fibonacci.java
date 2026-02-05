cffa48961a3b2dcee7a43c5676f8c7faea974c3e
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-05 14:41:30 +0530

    fibonacci

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,20 @@
+package Recursion;
+
+public class Fibonacci {
+    public int fib(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int fnm1=fib(n-1);
+        int fnm2=fib(n-2);
+        int fn = fnm1+fnm2;
+        return fn;
+        
+    }
+    public static void main(String[] args) {
+        int n=6;
+        Fibonacci obj = new Fibonacci();
+        int fibnum = obj.fib(n);
+        System.out.println(fibnum);
+    }
+}
