beec48cd295890d21fddae8135aea4495ebdba86
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-10 14:17:11 +0530

    added

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,7 +1,34 @@
 package Sorting;
 
 public class InsertionSort {
+    public static void insertionSort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev= i-1;
+            //find 
+            while (prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //insert 
+            arr[prev +1]=curr;
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
     public static void main(String[] args) {
-        
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.println("Before Sorting:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("\nAfter Sorting:");
+        printArray(arr);
     }
 }
