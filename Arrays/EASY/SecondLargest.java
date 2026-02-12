e402e3365fc61fa1e6df806bccb8af42f3385a7d
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-12 12:07:37 +0530

    added

---
+++ Arrays/EASY/SecondLargest.java
@@ -0,0 +1,29 @@
+package Arrays.EASY;
+
+public class SecondLargest {
+    public static void secondLargest(int arr[]) {
+        int ms1 = Integer.MIN_VALUE, ms2 = Integer.MIN_VALUE;
+        int sm1 = Integer.MAX_VALUE, sm2 = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            ms1=Math.max(ms1, arr[i]);
+            sm1=Math.min(sm1, arr[i]);
+        }
+        for(int i=0;i<arr.length;i++){
+            if(sm2>arr[i] && arr[i]!=sm1){
+                sm2=arr[i];
+            }
+            if(ms2<arr[i] && arr[i]!=ms1){
+                ms2=arr[i];
+            }
+            
+        }
+        System.out.println("Second smallest is " + sm2);
+        System.out.println("Second largest is " + ms2);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 5, 4, 6, 9, 4 };
+        secondLargest(arr);
+
+    }
+}
