c352522421e013fd145a7399a311c0ba05c1d723
shubhamgupta780048@gmail.com <shubhamgupta780048@gmail.com>
2026-02-24 16:13:55 +0530

    ll

---
+++ LinkedList/createLL.java
@@ -0,0 +1,73 @@
+package LinkedList;
+
+public class createLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+     
+    public void addFirst(int data){
+        //step 1 = create new node 
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head=tail=newNode; 
+            return ;
+        }
+
+        // step 2 = newNode next = head
+
+        newNode.next=head;
+        //step 3 head = newnode 
+
+        head=newNode;
+    }
+
+    public void addLast(int data){
+        //step 1 create new node 
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head=tail=newNode; 
+            return ;
+        }
+        // step 2 
+        tail.next=newNode;
+        //step 3 
+        tail = newNode;
+    }
+    public void print(){
+        if(head==null){
+            System.out.println("LL is empty");
+        }
+        Node temp = head;
+        while (temp!=null){
+            System.out.print(temp.data);
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        createLL ll=new createLL();
+        ll.print();
+        ll.addFirst(2);
+        ll.print();
+        ll.addFirst(1);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
+
+
+    }
+
+}
